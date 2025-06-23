890d440618114f51a1720b0a30814970c0228f17
unknown <tupm@devcamp.edu.vn>
2025-06-23 15:54:24 +0700

    Fix update

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -12,18 +12,18 @@ import {
 import { protect, admin, optionalAuth } from "../middlewares/authMiddleware.js";
 
 // Tạo đơn hàng mới (cho cả user và guest)
-router.post("/", optionalAuth, createOrder);
+router.route("/").post(optionalAuth, createOrder);
 
 // Lấy danh sách đơn hàng của user đang đăng nhập
-router.get("/myorders", protect, getMyOrders);
+router.route("/myorders", protect, getMyOrders);
 
 // Lấy chi tiết một đơn hàng bằng ID
-router.get("/:id", protect, getOrderById);
+router.route("/:id").get(protect, getOrderById);
 
 // Cập nhật trạng thái đã thanh toán
-router.put("/:id/pay", protect, updateOrderToPaid);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 
 // Cập nhật trạng thái đã giao hàng (chỉ Admin)
-router.put("/:id/deliver", protect, admin, updateOrderToDelivered);
+router.route("/:id/deliver").put(protect, admin, updateOrderToDelivered);
 
 export default router;
